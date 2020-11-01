47abcb7eedba1f7e6becc88d4a0f3fa506dfa099
SamsunNaher <naher.iu.10@gmail.com>
2020-10-31 22:45:20 -0600

    new

---
+++ testing-R-Studio using vectors.R
@@ -0,0 +1,7 @@
+#Codes for creating vectors using c()function()
+
+z<-c(1, 2)          #mumeric
+Z<c(TRUE, FALSE)    # logical
+Z<-c("x", "y", "z") # character
+z<-1:10             # interger
+z<-c(1+0i, 2_1i)    # complex
